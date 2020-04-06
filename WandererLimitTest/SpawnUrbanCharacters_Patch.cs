a648e26a028494e060987357820ed4ff0073f351
Pol Blanes <polblanes@gmail.com>
2020-04-06 23:10:32 +0200

    First Commit

---
+++ WandererLimitTest/SpawnUrbanCharacters_Patch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using TaleWorlds.CampaignSystem.SandBox.CampaignBehaviors;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.Core;
+
+using System.Windows.Forms;
+using System.Reflection.Emit;
+
+using System.IO;
+
+namespace WandererLimitTest
+{
+    [HarmonyPatch(typeof(UrbanCharactersCampaignBehavior))]
+    [HarmonyPatch("SpawnUrbanCharacters")]
+    public static class SpawnUrbanCharacters_Patch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+            codes.RemoveRange(141, 3);
+            return codes.AsEnumerable();
+        }
+    }
+}
---
+++ WandererLimitTest/WandererLimitTestSubModule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TaleWorlds.Core;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+
+using HarmonyLib;
+using System.Windows.Forms;
+
+namespace WandererLimitTest
+{
+    public class WandererLimitTestSubModule : MBSubModuleBase
+    {
+        protected override void OnSubModuleLoad()
+        {
+            base.OnSubModuleLoad();
+            try
+            {
+                new Harmony("mod.bannerlord.wandererlimittest").PatchAll();
+            }
+            catch (Exception ex)
+            {
+                string str = "Error patching:\n";
+                string message = ex.Message;
+                string str2 = " \n\n";
+                Exception innerException = ex.InnerException;
+                MessageBox.Show(str + message + str2 + ((innerException != null) ? innerException.Message : null));
+            }
+        }
+    }
+}
